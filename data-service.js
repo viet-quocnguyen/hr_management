65ed16974930b95acdd34ab7528d26bf76d27efb
qvnguyen <nguyenquocviet170898@gmail.com>
2019-09-24 22:36:28 -0400

    Finish assignment 2

--- data-service.js
+++ data-service.js
@@ -1,13 +1,52 @@
-const fs = require('fs');
+const fs = require("fs");
 
 var employees = [];
 var departments = [];
 
-module.exports.initialize = () => {
+module.exports.initialize = function() {
+  return new Promise((resolve, reject) => {
     fs.readFile("./data/employees.json", (err, data) => {
-        if (err) throw err;
-        employees = JSON.parse(data);
-        
-        return employees;
-    })
-}
+      if (err) reject("Unable to read file");
+      employees = JSON.parse(data);
+    });
+
+    fs.readFile("./data/departments.json", (err, data) => {
+      if (err) reject("Unable to read file");
+      departments = JSON.parse(data);
+    });
+    resolve("Read file successfully");
+  });
+};
+
+module.exports.getAllEmployees = function() {
+  return new Promise((resolve, reject) => {
+    if (employees.length === 0) {
+      reject("No results returned");
+    }
+    resolve(employees);
+  });
+};
+
+module.exports.getManagers = function() {
+  var managers = [];
+  employees.forEach(employee => {
+    if (employee.isManager === true) {
+      managers.push(employee);
+    }
+  });
+  return new Promise((resolve, reject) => {
+    if (managers.length === 0) {
+      reject("No results returned");
+    }
+    resolve(managers);
+  });
+};
+
+module.exports.getDepartments = function() {
+  return new Promise((resolve, reject) => {
+    if (departments.length === 0) {
+      reject("No results returned");
+    }
+    resolve(departments);
+  });
+};
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 /*********************************************************************************
- * WEB322 – Assignment 1
- * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
- * No part of this assignment has been copied manually or electronically from any other source
- * (including web sites) or distributed to other students.
+ * WEB322 – Assignment 02
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+ * of this assignment has been copied manually or electronically from any other source
+ * (including 3rd party web sites) or distributed to other students.
  *
  * Name: Quoc Viet Nguyen
  * Student ID: 107724189
- * Date: Sept 08,2019
+ * Date: September 24th, 2019
  *
- * Online (Heroku) URL: https://web322-qvnguyen.herokuapp.com
+ * Online (Heroku) Link: https://web322-qvnguyen.herokuapp.com/
  *
  ********************************************************************************/
-var data = require("./data-service.js");
+
 var employeesData = require("./data/employees.json");
 var departmentsData = require("./data/departments.json");
 var dataService = require("./data-service.js");
@@ -19,42 +19,62 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-
-
-
 var HTTP_PORT = process.env.PORT || 8080;
-app.use(express.static('public'));
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  
   res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
 app.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/about.html"));
-})
+});
 
 app.get("/employees", (req, res) => {
-  res.json(employeesData);
-})
+  dataService
+    .getAllEmployees()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json({ message: err });
+    });
+});
 
 app.get("/departments", (req, res) => {
-  res.json(departmentsData);
-})
+  dataService
+    .getDepartments()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json({ message: err });
+    });
+});
 
 app.get("/managers", (req, res) => {
-  var managersData = [];
-  employeesData.forEach((employee) => {
-    if(employee.isManager === true){
-      managersData.push(employee);
-    }
-  })
-  res.json(managersData);
-})
+  dataService
+    .getManagers()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json({ message: err });
+    });
+});
 
 app.use((req, res) => {
   res.status(404).send("Page not found");
-})
-
-app.listen(HTTP_PORT, () => {
-  console.log(`Express http server listening on ${HTTP_PORT}`);
 });
+
+dataService
+  .initialize()
+  .then(res => {
+    app.listen(HTTP_PORT, () => {
+      console.log(`Express http server listening on ${HTTP_PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
